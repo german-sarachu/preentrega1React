46bf88e92fa9cce542ca5778ec07a5d845f48075
German Sarachu <germansarachubrusco@hotmail.com>
2024-05-30 19:12:45 -0300

    probando firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import Layout from "./components/Layout/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import CartProvider from "./contexts/CartProvider";
 import CartContainer from "./components/CartContainer/CartContainer";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  snapshotEqual,
+} from "firebase/firestore";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const db = getFirestore();
+
+    const ProductsCollection = collection(db, "products");
+    getDocs(ProductsCollection).then((snapshot) => {
+      setProducts(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
   return (
-    <BrowserRouter>
-      <CartProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<ItemListContainer />} />
-            <Route
-              path="/category/:categoryName"
-              element={<ItemListContainer />}
-            />
-            <Route
-              path="/product/:productId"
-              element={<ItemDetailContainer />}
-            />
-            <Route path="/cart" element={<CartContainer />} />
-            <Route path="/checkout" element={<h1>Proximamente...</h1>} />
-          </Routes>
-        </Layout>
-      </CartProvider>
-    </BrowserRouter>
+    <>
+      <div>
+        {products.map((product) => {
+          <div key={product.id}>
+            <img src={product.img} alt={product.title} />
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+          </div>;
+        })}
+      </div>
+
+      {/*<BrowserRouter>
+        <CartProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<ItemListContainer />} />
+              <Route
+                path="/category/:categoryName"
+                element={<ItemListContainer />}
+              />
+              <Route
+                path="/product/:productId"
+                element={<ItemDetailContainer />}
+              />
+              <Route path="/cart" element={<CartContainer />} />
+              <Route path="/checkout" element={<h1>Proximamente...</h1>} />
+            </Routes>
+          </Layout>
+        </CartProvider>
+      </BrowserRouter>*/}
+    </>
   );
 }
-
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,18 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkhGK9t1Xj1tSrbU1aMhcTjy35zwT7Scw",
+  authDomain: "lanuevaespiga-ee440.firebaseapp.com",
+  projectId: "lanuevaespiga-ee440",
+  storageBucket: "lanuevaespiga-ee440.appspot.com",
+  messagingSenderId: "1051519444170",
+  appId: "1:1051519444170:web:44798e5cb38561d6f0c033"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
